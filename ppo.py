8868aa36a78b04dd3311c2f30ced8a7d51b2b082
Miguel-kjh <miguelangel.kjh@gmial.com>
2024-06-19 19:53:51 +0100

    udpate

--- ppo.py
+++ ppo.py
@@ -1,8 +1,11 @@
 import argparse
 import os
 from distutils.util import strtobool
+import gym
 from torch.utils.tensorboard import SummaryWriter
 import time
+from lightning import seed_everything
+import torch
 
 def parse_args():
     parse = argparse.ArgumentParser()
@@ -28,26 +31,37 @@ if __name__ == "__main__":
     args = parse_args()
     print(args)
     run_name = f'{args.exp_name}_{args.gym_id}_{args.seed}_{int(time.time())}'
+    root_dir = os.path.join("runs", run_name)
     if args.track:
         import wandb
+        wandb.tensorboard.patch(root_logdir=root_dir)
         wandb.init(
             project=args.wandb_project_name, 
             entity=args.wandb_entity, 
             name=run_name,
-            sync_tensorboard=True,
             config=vars(args),
             monitor_gym=True,
             save_code=True,
         )
-    writer = SummaryWriter(f'./runs/{run_name}')
+    writer = SummaryWriter(root_dir)
     writer.add_text(
         "hyperparameters",
         "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{arg}|{getattr(args, arg)}|" for arg in vars(args)])),
     )
-    for i in range(100):
-        # train the agent here
-        # you can use `writer.add_scalar` to record the training process
-        writer.add_scalar('reward', i*2, i)
-    writer.close()
-    if args.track:
-        wandb.finish()
+    
+    seed_everything(args.seed)
+    device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
+
+    # vectorized environment
+    env = gym.make(args.gym_id)
+    env = gym.wrappers.RecordEpisodeStatistics(env)
+    env = gym.wrappers.RecordVideo(env, "videos", episode_trigger=lambda x: x % 100 == 0)
+    observation = env.reset()
+    episodic_return = 0
+    for _ in range(200):
+        action = env.action_space.sample()
+        observation, reward, done, _, info = env.step(action)
+        if done:
+            print(f"Episodic return: {info['episode']['r']}")
+            observation = env.reset()
+    env.close()
